1c224eb91eec94074872eb7acc8aeaee8da0b496
zach <zach.rosen@yahoo.com>
2022-06-01 11:26:56 -0400

    finalized starter code

--- scripts/deploy_nft.py
+++
@@ -1,8 +0,0 @@
-from brownie import BrydgeCollection, accounts, config
-
-
-def main():
-    dev = accounts.add(config['wallets']['from_key'])
-    BrydgeCollection.deploy(
-        {'from': dev}
-    )
