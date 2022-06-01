1aae8ed84af7385febdec07b464041fc79910617
zach <zach.rosen@yahoo.com>
2022-06-01 11:26:00 -0400

    added dummy variable to nftContractAddress in config.js

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
 //brydge nft contract deployed on rinkeby
-export const nftContractAddress = '0x9704DB9d9aa6aa3d7e01E0863f2825B105B4Ff85'
+export const nftContractAddress = 'YourNFTContractAddressHere'
